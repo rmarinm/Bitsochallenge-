ce154ad66e4355f81f960a1da6c8cf573c968ac0
rmarinm <54190862+rmarinm@users.noreply.github.com>
2024-11-11 15:51:35 -0600

    Update ETL_export_data.py

--- ETL_export_data.py
+++ ETL_export_data.py
@@ -2,7 +2,7 @@ import psycopg2
 import pandas as pd
 from config import DWH_DB_HOST, DWH_DB_NAME, DWH_DB_USER, DWH_DB_PASSWORD, DWH_DB_SCHEMA
 
-# Conexión al DWH
+# Connection to DWH
 conn = psycopg2.connect(
     host=DWH_DB_HOST,
     database=DWH_DB_NAME,
@@ -11,14 +11,14 @@ conn = psycopg2.connect(
     options=f'-c search_path={DWH_DB_SCHEMA}'
 )
 
-# Función para exportar tablas a CSV
+# Function to export tables to CSV
 def export_table_to_csv(table_name):
     query = f"SELECT * FROM {table_name}"
     df = pd.read_sql(query, conn)
     df.to_csv(f"{table_name}.csv", index=False)
-    print(f"Datos exportados de {table_name} a {table_name}.csv")
+    print(f"Data exported from {table_name} to {table_name}.csv")
 
-# Lista de tablas a exportar
+# List of tables to export
 tables = ["Dim_User", "Dim_Jurisdiction", "Dim_UserLevel", "Dim_Product", "Dim_Date", "Fact_Transactions", "Fact_Events", "Fact_Activities"]
 
 for table in tables:
@@ -26,4 +26,4 @@ for table in tables:
 
 # Cerrar conexión
 conn.close()
-print("Exportación a CSV completa.")
+print("Full CSV export.")
